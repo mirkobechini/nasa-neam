c4b323ea329c4444fced98dbb6d62146560662dd
mirkobechini <mirkobechini@gmail.com>
2026-07-05 18:04:22 +0200

    refactor: remove time range filter from navbar wrapper

--- src/components/layout/navbar-wrapper.tsx
+++ src/components/layout/navbar-wrapper.tsx
@@ -1,17 +1,7 @@
 "use client";
 
 import { Navbar } from "@/components/layout/navbar";
-import { TimeRangeFilter } from "@/components/dashboard/time-range-filter";
 
 export function NavbarWrapper() {
-    return (
-        <>
-            <Navbar />
-            <div className="bg-background/50 border-b border-border px-4 md:px-6 py-3">
-                <div className="max-w-7xl mx-auto">
-                    <TimeRangeFilter />
-                </div>
-            </div>
-        </>
-    );
+    return <Navbar />;
 }
